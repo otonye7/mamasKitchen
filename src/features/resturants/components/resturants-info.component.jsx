dcef53a4c69c61536ddc8a19d3bcf2b941e7a127
otonye <otonyeamie@gmail.com>
2021-06-08 20:31:26 +0100

    master

--- src/features/resturants/components/resturants-info.component.jsx
+++ src/features/resturants/components/resturants-info.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import { Card } from 'react-native-paper';
+import {SvgXml} from 'react-native-svg';
+import star from '../../../../assets/star';
+import open from '../../../../assets/open';
 
 
 const Title = styled.Text `
@@ -25,6 +28,28 @@ const Info = styled.View `
   padding: ${(props) => props.theme.space[3]};
 `
 
+const Rating = styled.View `
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`
+
+const Section = styled.View `
+  flex-direction: row;
+  align-items: center;
+`;
+
+
+const SectionEnd = styled.View `
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Open = styled(SvgXml)`
+  flex-direction: row;
+`;
+
 
 const ResturantInfo = ({resturant = {}}) => {
     const {
@@ -36,11 +61,28 @@ const ResturantInfo = ({resturant = {}}) => {
         rating = 4,
         isClosedTemporarily
     } = resturant
+    
+    const ratingArray = Array.from(new Array(Math.floor(rating)));
+
     return (
         <ResturantCard elevation={5} >
             <ResturantCardCover key={name} source={{uri: photos[0] }} />
             <Info>
                <Title>{name}</Title>
+               <Section>
+               <Rating>
+               {
+                   ratingArray.map(() => (
+                       <SvgXml xml={star} width={20} height={20}/>
+                   ))
+               }
+               </Rating>
+               <SectionEnd>
+                   {
+                       isOpenNow && <Open xml={open} width={20} height={20}/>
+                   }
+               </SectionEnd>
+               </Section>
                <Address>{address}</Address>
             </Info>
         </ResturantCard>
