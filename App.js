c9b4fef1a717b10d29a0ce5ab7b897b240365e49
otonye <otonyeamie@gmail.com>
2021-06-08 22:01:28 +0100

    master

--- App.js
+++ App.js
@@ -2,10 +2,24 @@ import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import {theme} from './src/infrastructure/theme';
+import {Text} from 'react-native';
 import ResturantScreen from './src/features/resturants/screens/resturants.screens';
 import {useFonts as useOswald, Oswald_400Regular} from '@expo-google-fonts/oswald';
 import {useFonts as useLato, Lato_400Regular} from '@expo-google-fonts/lato';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { Ionicons } from "@expo/vector-icons";
 
+const Tab = createBottomTabNavigator();
+
+const Setting = () => (
+  <Text>Hey</Text>
+)
+
+const Map = () => (
+  <Text>Hey</Text>
+)
+ 
 export default function App() {
 
   const [oswaldLoaded] = useOswald({
@@ -23,7 +37,34 @@ export default function App() {
   return (
    <>
    <ThemeProvider theme={theme}>
-      <ResturantScreen/>
+      <NavigationContainer>
+        <Tab.Navigator  
+           screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+
+              if (route.name === "Resturants") {
+                iconName = "md-restaurant";
+              } else if (route.name === "Setting") {
+                iconName = "md-settings";
+              } else if (route.name === "Map") {
+                iconName = "md-map";
+              }
+
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: "tomato",
+            inactiveTintColor: "gray",
+          }}
+        >
+          <Tab.Screen name='Resturants' component={ResturantScreen}/>
+          <Tab.Screen name='Map' component={Map}/>
+          <Tab.Screen name='Setting' component={Setting}/>
+        </Tab.Navigator>
+      </NavigationContainer>
    </ThemeProvider>
    <ExpoStatusBar style="auto" />
     </>
